d77ca97aa83cc8f35b57ec23422041e5675fa28b
Prakhar Chauhan <shinuc29@gmail.com>
2020-10-06 15:05:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,21 @@
-const config = require('mongodb').MongoClient;
-const uri = "mongodb+srv://prakhar:hum77hai%40online@cluster0.oubax.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: process.env.MONGODB_URI ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
+}
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://prakhar:hum77hai%40online@cluster0.oubax.mongodb.net/mernproject?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   client.close();
 });
-
+export default MongoClient
 export default config
