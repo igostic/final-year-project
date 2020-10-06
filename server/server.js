4107b9bbd3be7b22c4fae706db16b270f01f3875
Prakhar Chauhan <shinuc29@gmail.com>
2020-10-06 15:38:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose'
 // Connection URL
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://prakhar:<password>@cluster0.oubax.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://prakhar:hum77hai%40online@cluster0.oubax.mongodb.net/mernproject?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
