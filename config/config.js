bb2087152d0a1df9c796afd229e10a2206e09217
Prakhar Chauhan <shinuc29@gmail.com>
2020-10-06 15:28:48 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -8,13 +8,4 @@ const config = {
     (process.env.MONGO_PORT || '27017') +
     '/mernproject'
 }
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://prakhar:hum77hai%40online@cluster0.oubax.mongodb.net/mernproject?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 export default config
